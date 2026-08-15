1589eb290897f6d18a865039be030cb48f5da47f
yagnesh <vattiguntayagneshreddy@gmail.com>
2026-08-15 23:47:48 +0530

    added

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Support Insights Backend is running");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
+
