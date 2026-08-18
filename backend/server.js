8ebf0c6b72962c87003a7cd83a7f3f7fe5175586
yagnesh <vattiguntayagneshreddy@gmail.com>
2026-08-18 18:10:10 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -50,5 +50,5 @@ app.get("/api/tickets", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port  ${PORT}`);
 });
